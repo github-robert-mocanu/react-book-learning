8aacfff4604022884bbf8bf922cdd7283fb4bb61
Robert <robert_mihai2903@yahoo.com>
2021-11-26 11:46:02 +0200

    CSS Modules in React

--- react-book/src/App.js
+++ react-book/src/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import axios from 'axios'
-import './App.css'
+import styles from './App.module.css'
 
 const useSemiPersistentState = (key, initialState) => {
     const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
@@ -74,8 +74,8 @@ const App = () => {
     }
 
     return (
-        <div className={"container"}>
-            <h1 className={"headline-primary"}>My Hacker Stories</h1>
+        <div className={styles.container}>
+            <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
 
             <SearchForm searchTerm={searchTerm} onSearchInput={handleSearchInput} onSearchSubmit={handleSearchSubmit}/>
 
@@ -102,9 +102,9 @@ const InputWithLabel = ({id, children, value, type = "text", isFocused, onInputC
 
     return (
         <>
-            <label className={"label"} htmlFor={id}>{children} </label>
+            <label className={styles.label} htmlFor={id}>{children} </label>
             &nbsp;
-            <input className={"input"} ref={inputRef} id={id} type={type} value={value} autoFocus={isFocused} onChange={onInputChange}/>
+            <input className={styles.input} ref={inputRef} id={id} type={type} value={value} autoFocus={isFocused} onChange={onInputChange}/>
         </>
     )
 }
@@ -125,7 +125,7 @@ const List = ({list, onRemoveItem}) => {
 const Item = ({item, onRemoveItem}) => {
 
     return (
-        <li className={"item"}>
+        <li className={styles.item}>
         <span style={{width: '40%'}}>
            <a href={item.url}>{item.title}</a>
         </span>
@@ -133,7 +133,7 @@ const Item = ({item, onRemoveItem}) => {
             <span style={{width: '10%'}}>{item.num_comments}</span>
             <span style={{width: '10%'}}>{item.points}</span>
             <span style={{width: '10%'}}>
-                <button className={"button button-small"} type="button" onClick={() => onRemoveItem(item)}>
+                <button className={`${styles.button} ${styles.buttonSmall}`} type="button" onClick={() => onRemoveItem(item)}>
                     Dismiss
                 </button>
             </span>
@@ -142,12 +142,12 @@ const Item = ({item, onRemoveItem}) => {
 }
 
 const SearchForm = ({searchTerm, onSearchInput, onSearchSubmit}) => (
-    <form className={"search-form"} onSubmit={onSearchSubmit}>
+    <form className={styles.searchForm} onSubmit={onSearchSubmit}>
         <InputWithLabel id="search" value={searchTerm} onInputChange={onSearchInput} isFocused>
             <strong>Search:</strong>
         </InputWithLabel>
 
-        <button className={"button button_large"} type={"submit"} disabled={!searchTerm}>
+        <button className={`${styles.button} ${styles.buttonLarge}`} type={"submit"} disabled={!searchTerm}>
             Submit
         </button>
     </form>
