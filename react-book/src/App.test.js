a240563fe2cd0919cb6db9c67495c4c9895c8a98
Robert <robert_mihai2903@yahoo.com>
2021-12-02 13:00:33 +0200

    Testing in React

---
+++ react-book/src/App.test.js
@@ -0,0 +1,9 @@
+describe('somethig truthy and falsy',() => {
+    it('true to be true', () => {
+        expect(true).toBeTruthy()
+    });
+
+    it('false to be false', () => {
+        expect(false).toBeFalsy()
+    })
+})
